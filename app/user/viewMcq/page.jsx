92fc7f35f3058298ad7152391fb9bf4004b49f22
Ahsan Bashir <mahsanbashir104@gmail.com>
2024-11-13 16:05:08 +0500

    viewMcq table design updated

--- app/user/viewMcq/page.jsx
+++ app/user/viewMcq/page.jsx
@@ -46,11 +46,11 @@ alert("deleted.............")
 
       
       <table className='min-w-[900px] bg-gray-50'>
-        <thead>
-          <tr style={{ backgroundColor: '#f4f4f4' }}>
+        <thead >
+          <tr className='bg-blue-900 text-white border-2 border-blue-900' >
             <th style={styles.tableHeader} className='w-28'>Sr #</th>
-            <th style={styles.tableHeader} className='w-28'>Date</th>
-            <th style={styles.tableHeader} className='w-96'>Course</th>
+            <th style={styles.tableHeader} className='w-40'>Date</th>
+            <th style={styles.tableHeader} className='w-80'>Course</th>
             
             <th style={styles.tableHeader} className='w-96'>Questions</th>
             <th style={styles.tableHeader} className='w-96'>Answers</th>
@@ -62,9 +62,9 @@ alert("deleted.............")
         <tbody>
           {data && data.map((item, index) => (
             <tr key={index}>
-              <td style={styles.tableCell} className='w-28'>{`${count++}`}</td>
-              <td style={styles.tableCell} className='w-28'>{item.date.split(" ")[0].split("-").reverse().join("-")}</td>
-              <td style={styles.tableCell} className='w-96'>
+              <td style={styles.tableCell} className='w-28 text-center'>{`${count++}`}</td>
+              <td style={styles.tableCell} className='w-40'>{item.date.split(" ")[0].split("-").reverse().join("-")}</td>
+              <td style={styles.tableCell} className='w-80'>
               <span> <b>Subject :</b></span> {item.subject_name} <span> <br/><br/></span>
               {item.courses.map((course, i) => (
     <React.Fragment key={i}>
@@ -79,10 +79,10 @@ alert("deleted.............")
              
               <td style={styles.tableCell} className='w-96'>{item.question}</td>
               <td style={styles.tableCell} className='w-96'>
-               <span className='border-b border-gray-300'> <b> A: </b> {item.optA} <br/></span>
-               <span className='border-b border-gray-300'> <b> B: </b> {item.optB} <br/></span>
-               <span className='border-b border-gray-300'> <b> C: </b> {item.optC} <br/></span>
-               <span className='border-b border-gray-300'> <b> D: </b> {item.optD} </span>
+               <span className=' border-gray-300'> <b> A: </b> {item.optA} <br/></span>
+               <span className=' border-gray-300'> <b> B: </b> {item.optB} <br/></span>
+               <span className=' border-gray-300'> <b> C: </b> {item.optC} <br/></span>
+               <span className=' border-gray-300'> <b> D: </b> {item.optD} </span>
                <br/><br/>
                 <b>Correct Answer: </b>{item.answer}<br/>
 
@@ -90,7 +90,7 @@ alert("deleted.............")
              
               
               <td style={styles.tableCell} className='w-60'>{item.addedBy}</td>
-              <td style={styles.tableCell} className='w-32'><span className='text-red-500  text-sm hover:cursor-pointer' onClick={()=>deleteQuestion(item.id)}> Delete</span> </td>
+              <td style={styles.tableCell} className='w-32'><span className='text-red-500  text-sm hover:cursor-pointer' onClick={()=>deleteQuestion(item.id)}> Edit | Delete</span> </td>
               
             </tr>
           ))}
@@ -103,12 +103,7 @@ alert("deleted.............")
 
 // Inline CSS for table styling
 const styles = {
-  tableHeader: {
-    border: '1px solid #ddd',
-    padding: '8px',
-    backgroundColor: '#f2f2f2',
-    textAlign: 'left',
-  },
+
   tableCell: {
     border: '1px solid #ddd',
     padding: '8px',
